ca6398ac5ba7e7f5fa3a96f323291afc7145ee41
WILLOSU <william10sousamota@gmail.com>
2026-01-29 09:30:00 -0300

    chore: fix build scripts and environment mapping for production

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { SetupServer } from './server';
 import config from 'config';
 
-(async (): Promise<void> => {
-  const server = new SetupServer(config.get('App.port'));
+async function bootstrap() {
+  // config.get('App.port') lerá a variável PORT do Render graças ao Passo 3
+  const server = new SetupServer(config.get('App.port')); 
   await server.init();
   server.start();
-})();
+}
+
+bootstrap();
