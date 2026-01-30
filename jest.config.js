10b59e70bb3f6ccc4bbea94ee27f8fb3a69ef119
WILLOSU <william10sousamota@gmail.com>
2026-01-29 22:20:18 -0300

    fix: remove duplicate start method and fix port binding

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
-const { resolve } = require('path');
-const root = resolve(__dirname);
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+import { resolve } from 'path';
+const root = resolve(__dirname); // Pega a raiz onde o arquivo está
 
-module.exports = {
-  rootDir: root,
-  displayName: 'root-tests',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
-  testEnvironment: 'node',
-  clearMocks: true,
-  preset: 'ts-jest',
-  moduleNameMapper: {
-    '^@src/(.*)$': '<rootDir>/src/$1',
-    '^@test/(.*)$': '<rootDir>/test/$1',
-  },
+export const rootDir = root;
+export const displayName = 'root-tests';
+export const testMatch = ['<rootDir>/src/**/*.test.ts'];
+export const testEnvironment = 'node';
+export const clearMocks = true;
+export const preset = 'ts-jest';
+export const moduleNameMapper = {
+  // O mapeamento deve bater exatamente com o seu tsconfig.json
+  '^@src/(.*)$': '<rootDir>/src/$1',
+  '^@test/(.*)$': '<rootDir>/test/$1',
 };
