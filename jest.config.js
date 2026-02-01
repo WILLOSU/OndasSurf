2969402e1970c8790315923c5d180683bc282c8f
WILLOSU <william10sousamota@gmail.com>
2026-02-01 09:09:19 -0300

    ajustes

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 import { resolve } from 'path';
-const root = resolve(__dirname); // Pega a raiz onde o arquivo está
 
-export const rootDir = root;
-export const displayName = 'root-tests';
-export const testMatch = ['<rootDir>/src/**/*.test.ts'];
-export const testEnvironment = 'node';
-export const clearMocks = true;
-export const preset = 'ts-jest';
-export const moduleNameMapper = {
-  // O mapeamento deve bater exatamente com o seu tsconfig.json
-  '^@src/(.*)$': '<rootDir>/src/$1',
-  '^@test/(.*)$': '<rootDir>/test/$1',
+// 🔹 sobe um nível (raiz do projeto)
+const root = resolve(__dirname, '..');
+
+export default {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
 };
