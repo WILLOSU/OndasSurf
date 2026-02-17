3f53c7f58fed455dd23afe8dfbed8f1b92f2754a
WILLOSU <william10sousamota@gmail.com>
2026-02-17 13:35:37 -0300

    limite stormglass

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,4 +1,4 @@
-import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get, Middleware } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Forecast } from '@src/services/forecast';
 import { Beach } from '@src/models/beach';
@@ -6,26 +6,46 @@ import { StormGlass } from '@src/clients/stormGlass';
 import { authMiddleware } from '@src/middlewares/auth';
 import logger from '@src/logger';
 import { BaseController } from '.';
+import rateLimit from 'express-rate-limit';
+import ApiError from '@src/util/errors/api-error';
 
 const stormGlass = new StormGlass();
 const forecast = new Forecast(stormGlass);
 
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute in milliseconds
+  max: 10,
+  handler(_, res: Response): void {
+    res.status(429).send(
+      ApiError.format({
+        code: 429,
+        message: "Too many requests to the '/forecast endpoint'",
+      })
+    );
+  },
+});
+
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
-export class ForecastController extends BaseController{
+export class ForecastController extends BaseController {
   @Get('')
+  @Middleware(rateLimiter)
   public async getForecastForLoggedUser(
     req: Request,
     res: Response
   ): Promise<void> {
     try {
-      const beaches = await Beach.find({user: req.decoded?.id});
-      const forecastData = await forecast.processForecastForBeaches(beaches as unknown as Beach[]);
+      const beaches = await Beach.find({ user: req.decoded?.id });
+      const forecastData = await forecast.processForecastForBeaches(
+        beaches as unknown as Beach[]
+      );
       res.status(200).send(forecastData);
-    } catch (error){
+    } catch (error) {
       logger.error({ error }, 'Forecast error');
-      this.sendErrorResponse(res, {code: 500, message: 'Something went wrong'})
-      
+      this.sendErrorResponse(res, {
+        code: 500,
+        message: 'Something went wrong',
+      });
     }
   }
 }
